9bd41fc08cfe5164cf6f52e0a373f79cd719fdbd
Andrii-Vekker <vekker304@gmail.com>
2022-06-28 21:59:38 +0300

    цууцк

--- js/index.js
+++ js/index.js
@@ -25,16 +25,16 @@
 //     return pin.length === 4 || pin.length === 6;
 // }
 ///////////////////////////////////////////five///////////////////////////////////
-function validatePIN (pin) {
-  if (pin.length === 4 || pin.length ===6) {
-    return pin.split('').every((num) => !isNaN(parseInt(num)));
-  }
-  return false;
-}
+// function validatePIN (pin) {
+//   if (pin.length === 4 || pin.length ===6) {
+//     return pin.split('').every((num) => !isNaN(parseInt(num)));
+//   }
+//   return false;
+// }
 
-console.log(validatePIN("00.0"))
-console.log(validatePIN("1233"))
-console.log(validatePIN("123342"))
+// console.log(validatePIN("00.0"))
+// console.log(validatePIN("1233"))
+// console.log(validatePIN("123342"))
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // const numbers = [12, 3, 4, 5, 67]
 // const sum = numbers.reduce((sum, el) => sum + el)
@@ -60,3 +60,62 @@ console.log(validatePIN("123342"))
 // }
 
 // console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+
+// ///////////////////////////////////Завершите функцию, которая принимает строковый параметр и переворачивает
+// каждое слово в строке.Все пробелы в строке должны быть сохранены///////////////////////////////////////////
+// function reverseWords(str) {
+//  console.log(str.split(" ").map(e => e.split("").reverse().join("")).join(" "))
+// }
+/////////////////////second//////////////////////
+
+// function reverseWords(str) {
+  
+//   str = str.split(" ");
+  
+//   var res = [];
+  
+//   for (var i = 0; i < str.length; i++){
+//       res.push (str[i].split("").reverse().join(""));
+//   }
+  
+//   return res.join(" ");
+// }
+////////////////////////////third///////////////////////////////////////////
+// function reverseWords(str) {
+//   // Go for it
+//   //split words into seperate arrays
+//   return str.split("").reverse().join("").split(" ").reverse().join(" ");
+// }
+// console.log(reverseWords("This is an example!"))
+// console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
+
+//////////////////////////////////////////
+// Учитывая строку слов, вам нужно найти слово с наивысшим баллом.
+// Каждая буква слова оценивается в баллах в соответствии с ее положением в алфавите: a = 1, b = 2, c = 3и т. д.
+//////////////////////////////////////////////////////////////////////
+// function alphabetPosition(text) {
+//   const newStr = text.toUpperCase().replace(/[^a-zа-яё]/gi, "").split("").join("")
+  
+//   let total = [];
+//   for (let i = 0; i < newStr.length; i += 1) {
+//     const num = newStr.charCodeAt(i) - 64;
+// total.push(num)
+// }
+// return total.join(" ")
+// }
+
+
+function high(x) {
+    const str = x.toUpperCase().replace(/[^a-zа-яё]/gi, " ").split(" ")
+    console.log(str)
+    const arr = []
+    for (let i = 0; i < str.length; i+=1) {
+        const wordArr = str.map((e, i, arr) => e.split())
+        console.log(wordArr.map(v => console.log(v.reduce(acc, val, i, arr) => val[i]), 0))
+    
+
+    }
+
+}
+
+console.log(high(('man i need a taxi up to ubud')))
