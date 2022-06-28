7de39abb86f1335fb220dc3cf84e4b7306abbc44
Andrii-Vekker <vekker304@gmail.com>
2022-06-29 00:24:31 +0300

    wqw

--- js/index.js
+++ js/index.js
@@ -89,33 +89,59 @@
 // console.log(reverseWords("This is an example!"))
 // console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
 
-//////////////////////////////////////////
+//////////////////////////////////////////task
 // Учитывая строку слов, вам нужно найти слово с наивысшим баллом.
 // Каждая буква слова оценивается в баллах в соответствии с ее положением в алфавите: a = 1, b = 2, c = 3и т. д.
-//////////////////////////////////////////////////////////////////////
+
 // function alphabetPosition(text) {
-//   const newStr = text.toUpperCase().replace(/[^a-zа-яё]/gi, "").split("").join("")
+//   const alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   
-//   let total = [];
-//   for (let i = 0; i < newStr.length; i += 1) {
-//     const num = newStr.charCodeAt(i) - 64;
-// total.push(num)
+//    const textSplit = text.toLowerCase().split(" ")
+//    let array = [];
+//   for (let i = 0; i < textSplit.length; i += 1) {
+//     const word = textSplit[i].replace(/[^a-zа-яё]/gi, "").split("")
+//     const indexLetter = word.reduce((summ, letter) => (alph.indexOf(letter) + 1) + summ, 0)
+//     console.log(indexLetter)
+//     array.push(indexLetter)
+//   }
+//   return  textSplit.filter(w => array.indexOf(Math.max(...array)) === textSplit.indexOf(w))
 // }
-// return total.join(" ")
+// console.log(alphabetPosition("The word is ended soon"))
+
+///////////////////////////////////////////////////////task///////////////////////////////////////////////
+// По городу движется автобус, и на каждой остановке он берет и высаживает несколько человек.
+// Вам предоставляется список(или массив) целочисленных пар.Элементы каждой пары представляют количество людей, садящихся в
+// автобус(первый элемент) и количество людей, выходящих из автобуса(второй элемент) на автобусной остановке.
+// Ваша задача — вернуть количество людей, оставшихся в автобусе после последней автобусной остановки(после последнего массива).
+// Несмотря на то, что это последняя автобусная остановка, автобус не пустой, и некоторые люди все еще в автобусе, и они, вероятно, спят там: D
+// Взгляните на тест-кейсы.
+// Имейте в виду, что тестовые примеры гарантируют, что количество людей в автобусе всегда >= 0. Таким образом,
+// возвращаемое целое число не может быть отрицательным.
+// Второе значение в первом целочисленном массиве равно 0, так как автобус на первой остановке пуст.
+// var number = function(busStops){
+//   const pass = busStops.map(e => (e[0] - e[1]))
+//   const res = pass.reduce((acc, v) => acc + v, 0)
+//   //////////////////////////////second
+//   const res2 = busStops.reduce((acc, prev, next, i, arr) => acc + (prev[0] - prev[1]), 0)
+//   return res2
 // }
 
 
-function high(x) {
-    const str = x.toUpperCase().replace(/[^a-zа-яё]/gi, " ").split(" ")
-    console.log(str)
-    const arr = []
-    for (let i = 0; i < str.length; i+=1) {
-        const wordArr = str.map((e, i, arr) => e.split())
-        console.log(wordArr.map(v => console.log(v.reduce(acc, val, i, arr) => val[i]), 0))
-    
+// console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
 
-    }
+/////////////////////////////////////////////////////task
+// function positiveSum(arr) {
+//   return arr.filter(e => e > 0).reduce((acc, v) => acc + v, 0)
+////////////////////////////second
+// return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+// }
 
+// console.log(positiveSum([1,-2,3,4,5]))
+
+/////////////////////////////////////task
+function betterThanAverage(classPoints, yourPoints) {
+  console.log(classPoints.reduce((acc, e, arr) => (acc + e) / arr.length))
+  
 }
 
-console.log(high(('man i need a taxi up to ubud')))
+console.log(betterThanAverage([2, 3], 5))
